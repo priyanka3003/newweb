26a66db08cc84e6c7efb6c300499c0b1ccc70558
priyanka3003 <46740515+priyanka3003@users.noreply.github.com>
2022-08-09 22:53:26 -0400

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,49 +5,31 @@ const Hero = () => {
     <div className="hero">
       <Container className="justify-content-center">
         <Carousel>
-          <Carousel.Item interval={1000}>
+          <Carousel.Item interval={3000}>
             <img
               className="d-block w-100"
               src="https://static1.colliderimages.com/wordpress/wp-content/uploads/slice_inception_movie_poster_banner_01.jpg?q=50&fit=contain&w=1500&h=&dpr=1.5"
               alt="First slide"
               width={1000}
-              height={400}
+              height={300}
             />
           </Carousel.Item>
-          <Carousel.Item interval={1000}>
+          <Carousel.Item interval={3000}>
             <img
               className="d-block w-100"
-              src="../image/static/actor2.jpg"
+              src="https://miro.medium.com/proxy/1*h8boORPeJG-t9Y3kEDiGzg.png"
               alt="Second slide"
               width={1000}
-              height={400}
+              height={300}
             />
           </Carousel.Item>
-          <Carousel.Item interval={1000}>
+          <Carousel.Item interval={3000}>
             <img
               className="d-block w-100"
-              src="../image/static/actor3.jpg"
+              src="https://www.justwatch.com/images/backdrop/176190115/s640/the-promotion"
               alt="Second slide"
               width={1000}
-              height={400}
-            />
-          </Carousel.Item>
-          <Carousel.Item interval={1000}>
-            <img
-              className="d-block w-100"
-              src="../image/static/actor4.jpg"
-              alt="Second slide"
-              width={1000}
-              height={400}
-            />
-          </Carousel.Item>
-          <Carousel.Item interval={1000}>
-            <img
-              className="d-block w-100"
-              src="../image/static/actor5.jpg"
-              alt="Second slide"
-              width={1000}
-              height={400}
+              height={300}
             />
           </Carousel.Item>
         </Carousel>
