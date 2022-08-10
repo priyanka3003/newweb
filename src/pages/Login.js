8ba2c082942b3bb8df839dffcbc108df248d8e33
priyanka3003 <46740515+priyanka3003@users.noreply.github.com>
2022-08-10 02:18:06 -0400

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -11,7 +11,7 @@ const Login = () => {
     e.preventDefault()
     const login = { email, password }
 
-    fetch('http://localhost:8085/auth', {
+    fetch('https://mybackendweb.herokuapp.com/auth', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(login),
